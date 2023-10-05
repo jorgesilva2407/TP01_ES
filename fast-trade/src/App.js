7e413df96a1428a30804f39eea17d33e6c9f26ad
HenriqueDSousa <henriquedsousa122@gmail.com>
2023-10-05 14:35:59 -0300

    Versão inicial da pagina de venda

--- fast-trade/src/App.js
+++ fast-trade/src/App.js
@@ -6,11 +6,10 @@ import Register from './components/Register';
 import Login from './components/Login';
 import UserAccount from './components/UserAccount';
 import Chat from './components/Chat';
-<<<<<<< HEAD
+
 import Venda from './components/Venda';
-=======
 import Checkout from './components/Checkout';
->>>>>>> edf20f23ce63ce5b38404863335965e16a0c6375
+
 
 function App() {
 
@@ -26,12 +25,12 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/minhaconta" element={<UserAccount/>} />
           <Route path="/chat" element={<Chat/>} />
-<<<<<<< HEAD
+
           <Route path="/venda" element={<Venda/>} />
           
-=======
+
           <Route path='/checkout' element={<Checkout/>}/>
->>>>>>> edf20f23ce63ce5b38404863335965e16a0c6375
+
 
         </Routes>
       </Router>
