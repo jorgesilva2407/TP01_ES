d9f95b9e4e3e57b1dfaa49748cc0c7084d8c4385
Vitor-Git15 <vitoremanuelg147@gmail.com>
2023-10-05 14:20:12 -0300

    Adding checkout page

--- fast-trade/src/App.js
+++ fast-trade/src/App.js
@@ -6,6 +6,7 @@ import Register from './components/Register';
 import Login from './components/Login';
 import UserAccount from './components/UserAccount';
 import Chat from './components/Chat';
+import Checkout from './components/Checkout';
 
 function App() {
 
@@ -21,6 +22,7 @@ function App() {
           <Route path="/login" element={<Login/>} />
           <Route path="/minhaconta" element={<UserAccount/>} />
           <Route path="/chat" element={<Chat/>} />
+          <Route path='/checkout' element={<Checkout/>}/>
 
         </Routes>
       </Router>
---
+++ fast-trade/src/components/Checkout.jsx
@@ -0,0 +1,99 @@
+import React, { useContext, useState } from "react";
+
+import AppContext from "../context/AppContext";
+import "../styles/Checkout.css";
+
+import Login from "./Login";
+import Header from "./Header";
+
+const Checkout = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [paymentMethod, setPaymentMethod] = useState('credit_card');
+
+  // if (!localStorage.getItem('username')) return <Login />
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+
+  const handlePaymentMethodChange = (e) => {
+    setPaymentMethod(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Aqui você pode adicionar a lógica para processar o pedido e enviar as informações do checkout para o servidor, incluindo o método de pagamento selecionado
+
+    // Limpa os campos após o envio
+    setName("");
+    setEmail("");
+    setAddress("");
+    setPaymentMethod("credit_card");
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="checkout-container">
+        <h2>Finalização de Compra</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={handleNameChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={handleEmailChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="address">Endereço de Entrega:</label>
+            <textarea
+              id="address"
+              value={address}
+              onChange={handleAddressChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="paymentMethod">Forma de Pagamento:</label>
+            <select
+              id="paymentMethod"
+              value={paymentMethod}
+              onChange={handlePaymentMethodChange}
+            >
+              <option value="credit_card">Cartão de Crédito</option>
+              <option value="paypal">PayPal</option>
+              <option value="bank_transfer">Transferência Bancária</option>
+            </select>
+          </div>
+          <button type="submit">Finalizar Compra</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
