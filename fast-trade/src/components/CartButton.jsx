584f024454ac1c5d85c34eb6da93db0639c42f47
jorgesilva <jorge.lima2407@gmail.com>
2023-10-05 16:35:51 -0300

    Correção estilo de botão

--- fast-trade/src/components/CartButton.jsx
+++ fast-trade/src/components/CartButton.jsx
@@ -4,19 +4,18 @@ import "../styles/CartButton.css"
 import Cart from "../icons/icons8-carrinho-de-compras-24.png";
 import AppContext from '../context/AppContext';
 
-function CartButton(){
+function CartButton() {
 
-    const {cartItems, isCartVisible, setIsCartVisible} = useContext(AppContext);
+    const { cartItems, isCartVisible, setIsCartVisible } = useContext(AppContext);
 
-    return(
-        <form type="button"
-        class="nav-item"
-        onClick={() => setIsCartVisible(!isCartVisible)}
+    return (
+        <button class="nav-item"
+            onClick={() => setIsCartVisible(!isCartVisible)}
         >
             <img src={Cart} alt="Cart" />
             {cartItems.length > 0 &&
-            <span className="cart-status">{cartItems.length}</span>}
-        </form>
+                <span className="cart-status">{cartItems.length}</span>}
+        </button>
     )
 
 }
