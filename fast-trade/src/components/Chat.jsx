a1d6c176a28b403859702ca55d49762871ce0e38
jorgesilva <jorge.lima2407@gmail.com>
2023-10-07 19:41:46 -0300

    Mudança do ID do chat

--- fast-trade/src/components/Chat.jsx
+++ fast-trade/src/components/Chat.jsx
@@ -14,7 +14,7 @@ function Chat() {
                 <ChatEngine
                     height="90vh"
                     projectID="d392b8ec-9c28-4780-a064-94a5ad330a52"
-                    userName={localStorage.getItem('name')}
+                    userName={localStorage.getItem('email')}
                     userSecret={localStorage.getItem('password')}>
                 </ChatEngine>
             </div>
